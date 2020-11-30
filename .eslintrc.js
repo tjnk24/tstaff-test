addf553c4b31b361d2d34f51bbb6b76ea778a64f
tjnk24 <tjnk24@gmail.com>
2020-11-30 12:01:51 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'no-unused-vars': 'warning',
+        'import/no-unresolved': 'off',
+        'import/extensions': 'off',
+        'react/jsx-filename-extension': [
+            'error',
+            {
+                extensions: [
+                    '.js',
+                    '.jsx',
+                    '.ts',
+                    '.tsx'
+                ]
+            }
+        ],
+    }
+};
---
+++ src/components/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+import TempDiv from './style';
+
+const App = () => (
+  <TempDiv>
+    Hello boilerplate!
+  </TempDiv>
+);
+
+export default App;
